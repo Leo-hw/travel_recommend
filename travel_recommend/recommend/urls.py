4eae4c8787aa383e32d2fe8476827f2d7840cd15
leo <lbh6314@gmail.com>
2021-03-29 12:13:38 +0900

    now on updating
    accounts/views.py, models.py, templates
    
    recommend/
    add cal_cf

--- travel_recommend/recommend/urls.py
+++ travel_recommend/recommend/urls.py
@@ -5,9 +5,14 @@ app_name = "recommend"
 print('urls실행')
 urlpatterns = [
     
+    # 리뷰 리스트 보기 - top5 여행지 인기도 순
     path('', Top5Site.as_view(), name = 'index'),
+    #
     path('reviewcreate/', ReviewCreate.as_view(), name = 'create'),
     path('reviewdelete/', ReviewDelete.as_view(), name = 'delete'),
-
-
+    path('reviewdetail/', ReviewCreate.as_view(), name = 'create'),
+    # 리뷰 like
+    path('reviewlike/', ReviewCreate.as_view(), name = 'create'),
+    path('search/', calculate, name = 'calculate'),
+    
 ]

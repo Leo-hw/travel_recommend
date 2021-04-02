b68dc8b8ed80a05442b9cf2c0ab760f618d8d9d6
leo <lbh6314@gmail.com>
2021-04-02 22:46:34 +0900

    update accounts/forms.py and templates for signup
    now fixing forms
    error : not enough values to unpack(expected 2 got 1)
     add city - ok
     add town - no
     add travel_sites - no
    
     noooooo
    and need to add cal_cnn for recommend
    add cal_cnn.py
     #120 line err, type err

--- travel_recommend/accounts/forms.py
+++ travel_recommend/accounts/forms.py
@@ -1,47 +1,135 @@
-from .widgets import starWidget
+from re import T
 from django import forms
+from django.forms import fields
 from recommend.models import Travel
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django import forms
 #from .models import User
+
+
+import MySQLdb
+config = {
+    'host':'127.0.0.1',
+    'user':'root',
+    'password':'123',
+    'database':'test',
+    'port':3306,
+    'charset':'utf8',
+    'use_unicode':True
+}
+conn = MySQLdb.connect(**config)
+
 class SignUpForm(forms.ModelForm):
     model = Travel
     
-    password = forms.CharField(label = 'password', widget=forms.PasswordInput, required= True)
-    password2 = forms.CharField(label='password2', widget=forms.PasswordInput, required= True)
-    rating = forms.IntegerField(label='rating', required = True, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    name  = forms.CharField(label='name', widget=forms.TextInput, required= True)    
-    city = forms.CharField(label = 'city', widget=forms.Select(
-        attrs={
-            
-           }
-        ))
-    town = forms.CharField(label = 'town', widget=forms.Select)
-    travel_name = forms.CharField(label = 'travel_name', widget=forms.Select(
-        attrs={
-            
-        }
-
-    ))
+    #cursor = conn.cursor()
+    town_list = []
+    trave_sites = []
     
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        towns = self.request.session['town']
+        traves = self.request.session['travel_site']
+        
+        print(towns)
+        for t in towns:
+            town_list.append(t['town'])
+        for tr in traves:
+            trave_sites.append(tr['travel_site'])
+        self.fields['town'].choices = town_list
+        self.fields['travel_site'].choices = trave_sites
+
+        return town_list, trave_sites
+
+    town = forms.ChoiceField(choices=town_list, required=True)
+    travel_site = forms.ChoiceField(choices=trave_sites)
+    # define fields 
+    username = forms.CharField(label = 'username', widget=forms.TextInput, required=True)
+    password = forms.CharField(label = 'password', widget=forms.PasswordInput, required= True)
+    repeat_password = forms.CharField(label='repeat_password', widget=forms.PasswordInput, required= True)
+    name  = forms.CharField(label='name', widget=forms.TextInput, required= True)
+    jumin = forms.IntegerField(label = 'jumin', required=True)
+    gen = forms.IntegerField(label='gen', required=True, validators=[MinValueValidator(1), MaxValueValidator(9)])
+    occupation = forms.IntegerField(label='occupation')
+    email = forms.EmailField(label='email', widget=forms.EmailInput)
+    rating = forms.IntegerField(label='rating', required = True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    
+    cities = list(Travel.objects.values_list('city', flat = True).distinct())
+    city = forms.CharField(label = 'city', widget=forms.Select(choices=cities))
+    #print(cities)
+    
+    # towns = list(Travel.objects.values_list('town', flat = True).distinct())
+    # town = forms.CharField(label = 'town', widget=forms.Select(choices=towns))
+    # print(towns)
+
+    # travel_sites = list(Travel.objects.values_list('name', flat = True).distinct())
+    # travel_site = forms.CharField(label = 'travel_site', widget=forms.Select(choices=travel_sites))
+    # print(travel_sites)
+    
+    """
+    class FooForm(forms.Form):
+    
+    def __init__(self, *args, **kwargs):
+        super(FooForm, self).__init__(*args, **kwargs)
+        bars = self.request.session['bars']
+        foo_list = []
+        for bar in bars:
+            foo_list.append((bar['id'], bar['name']),)
+        self.fields['foo'].choices = foo_list
+    foo = forms.ChoiceField(choices=foo_list, required=True)
+    """
+    
+    
+    '''
+    #도시
+    sql = "select distinct(city) from travel"
+    cursor.execute(sql)
+    conn.commit() 
+    
+    cits = cursor.fetchall()
+    cities = []
+    for c in cits:
+        cities.append(c)
+    #cities = 
+    city = forms.CharField(label = 'city', widget=forms.Select(choices=cities))
+    
+    #동
+    sql2 = "select distinct(town) from travel"
+    cursor.execute(sql2)
+    tows = cursor.fetchall()
+    towns = []
+    for t in tows:
+        towns.append(t)
+    #towns = cursor.fetchall()
+    town = forms.CharField(label = 'town', widget=forms.Select(choices=towns))
+    
+    #여행지명
+    sql3 = "select distinct(name) from travel"
+    cursor.execute(sql3)
+    #travel_sites = cursor.fetchall()
+    travel_sites = []
+    travel_s = cursor.fetchall()
+    for ts in travel_s:
+        travel_sites.append(ts)
+    travel_name = forms.CharField(label = 'travel_name', widget=forms.Select(choices=travel_sites))
+    
+    cursor.close()
+    conn.close()
+    '''
+
     class Meta:
         model = User
         
         # fill out fields with values for signup
         # can be added another field
-        #fields = ['username', 'password','password2', 'first_name', 'last_name', 'email', 'rating', 'city', 'town']
+        # fields = ['username', 'password','repeat_password', 'name','jumin','gen', 'occupation','email', 'rating', 'city', 'town', 'travel_name']
+        
+        # fields = ['username', 'password','repeat_password', 'name','jumin','gen', 'occupation','email', 'rating', 'city', 'town']
         fields = '__all__'
-        widgets = {
-            'rating': starWidget,
-        }
 
     def clean_Repeat_password(self):
         cd = self.cleaned_data
         if cd['password'] != cd['repeat_password']:
             raise forms.ValidationError('Your password is not correct')
         return cd['repeat_password']
-
-
-# 평점용
  
--- travel_recommend/recommend/cal_cnn.py
+++ travel_recommend/recommend/cal_cnn.py
@@ -12,85 +12,129 @@ from keras.regularizers import *
 from sklearn.metrics.pairwise import euclidean_distances
 
 # load data
-travel = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/data/placerating.csv")
+rating = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/data/placerating.csv")
+travel = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/data/국내여행지_중복제거_utf.csv")
 
 # define x, y 
-idx_reviewer = travel.groupby('userId').size().reset_index()
-print(idx_reviewer.shape)
+# 나이 
+age = []
+for i in range(1000):
+  nage=np.random.randint(20, 60)
+  age.append(nage)
+print(age)
+len(age)
+
+# 성별
+gen = []
+for i in range(1000):
+  ngen = np.random.randint(1,9)
+  
+  if ngen % 2 == 1:
+    ngens = '남'
+    
+  else:
+    ngens = '여'
+  gen.append(ngens)
+
+print(gen)
+
+# 직업
+occ = []
+for i in range(1000):
+  nocc = np.random.randint(1, 16)
+  #print(nocc)
+  if nocc == 1:
+    noccs = '학생'
+  elif nocc == 2:
+    noccs = '경영'
+  else:
+    noccs= '기타'
+  occ.append(noccs)
+print(occ)
+
+'''
+# 장르 추가는 따로 할 피료 없을듯
+genre=[]
+for i in range(1000):
+  genr = np.random.randint(0,80)
+  genre.append(genr)
+print(genre)
+'''
+
+# 사용자 데이터 프레임
+user = pd.DataFrame()
+user['user'] = rating['userId'].unique()
+user['genre'] = genre
+user['age'] = age
+user['occ'] = occ
+user['gen'] = gen
 
 
-user2idx = dict(zip(idx_reviewer.userId, idx_reviewer.index.values ))
-idx_places = travel.groupby('placeId').size().reset_index()
-idx_places.shape
+user.head()
+user = user.drop(columns='genre')
+user = user.rename(columns={'user' : 'userId'})
 
-place2idx = dict(zip(idx_places.placeId, idx_places.index.values))
-idx2place = dict(zip(idx_places.index.values, idx_places.placeId))
+# merge => for CF
+travel = travel.rename(columns={'Unnamed: 0' : 'placeId'})
+travel.drop('Unnamed: 8', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 9', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 10', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 11', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 12', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 13', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 14', axis = 1 ,inplace=True)
+travel.drop('Unnamed: 15', axis = 1 ,inplace=True)
 
-travel['place_idx'] = [place2idx.get(p) for p in travel.placeId]
-travel['user_idx'] = [user2idx.get(u) for u in travel.userId]
+travel_user = pd.merge(travel, user_rating,on = 'placeId')
+user_travel = pd.merge(user_rating, travel, on = 'placeId')
 
 
-travel.loc[:, ['user_idx', 'place_idx', 'rating']].values
+travel_user_rating = travel_user.pivot_table('rating', index='검색지명', columns='userId')
+user_travel_rating = user_travel.pivot_table('rating', index='userId', columns='검색지명')
 
-n_users = len(user2idx)
-n_places = len(place2idx)
+from sklearn.metrics.pairwise import cosine_similarity
+item_based_collabor = cosine_similarity(travel_user_rating)
+print(item_based_collabor.head())
 
-from keras.layers import Input, Embedding, LSTM, Dense
+
+def get_item_based_collabor(*args):
+      #item_based_collabor['args']
+  res = item_based_collabor.isin(args)
+  print(res)
+  result = res.sort_values(ascending = False)[:5] 
+  print(result)
+  return result
+get_item_based_collabor()
+
+
+### for CNN
+from keras.layers import Input, Embedding, LSTM, Dense, Conv1D, GlobalMaxPool1D
 from keras.models import Model
 import numpy as np
 
-sequence_length = 2000
+sequences_length = 2000
 embedding_demension = 1000
 
-user_in = Input(shape=(1,), dtype='int64', name='user_in')
-u = Embedding(n_users, 20, input_length=1,embeddings_regularizer=l2(1e-5))(user_in)
+# input err #120 TypeError: '<=' not supported between instances of 'str' and 'int'
+user_in = Input(shape=(5, ), dtype='int64', name = 'user_in')
+u = Embedding(user, 20, input_length=1, embeddings_regularizer=l2(1e-5))(user_in)
 u = Dense(64, activation='relu')(u)
 user_out = Dense(1, activation='sigmoid', name='user_out')(u)
 
-place_in = Input(shape=(1,), dtype='int64', name='place_in')
-p = Embedding(n_places, 20, input_length=1,embeddings_regularizer=l2(1e-5))(place_in)
+
+place_in = Input(shape=(16,), dtype='int64', name='place_in')
+p = Embedding(travel, 20, input_length=1,embeddings_regularizer=l2(1e-5))(place_in)
 p = Dense(64, activation='relu')(p)
 place_out = Dense(1, activation='sigmoid', name='place_out')(p)
 
+concat = Concatenate([user_out, place_out])
+concat = Dot(axes=2)([u,p])
+concat = Flatten(concat)
 
+concat = Dense(32, activation='relu')(concat)
+concat = BatchNormalization()(concat)
+con_out = Dense(1, activation='sigmoid')(concat)
 
-conc = Concatenate([user_out, place_out])
-conc = Dot(axes=2 )([u,p])
-print(type(conc))
-conc = Flatten()(conc)
-#conc = Reshape((1,u, p), input_shape=(user_in, place_in))(conc)
-#conc = Conv1D(filters=32,kernel_size=5 ,padding='valid')(conc)
-#conc = GlobalMaxPool1D()(conc)
-conc = Dense(32, activation='relu')(conc)
-conc = BatchNormalization()(conc)
-con_out = Dense(1, activation='sigmoid')(conc)
-
-# x = Dot(axes=2)([u,p])
-# x = Flatten()(x)
 model = Model([user_in, place_in], outputs=con_out)
-#model = Model([user_in, place_in], outputs=[user_out, place_out] )
-model.compile('Adam',loss='binary_crossentropy', metrics=['acc'])
-
+model.compile('Adam', loss = 'binary_crossentropy', metrics=['acc'])
 model.summary()
-
-from IPython.display import Image
-from keras.utils.vis_utils import model_to_dot
-
-Image(model_to_dot(model,show_shapes=True, show_layer_names=False).create(prog='dot', format='png'))
-
-data_set_mat = travel.loc[:, ['user_idx', 'place_idx', 'rating']].values
-
-history = model.fit(x=[data_set_mat[:,0], data_set_mat[:,1]], y=data_set_mat[:,2], 
-                    batch_size=64, epochs=10, validation_split=0.1, verbose=2)
-
-place_dist = euclidean_distances(X=model.layers[3].get_weights()[0])
-
-
-def get_place_recomm(beer_nm, place_dist, idx2place, topN=10):
-    q_b_idx = place2idx[beer_nm]
-    beer_dists = place_dist[q_b_idx]
-    orders = np.argsort(beer_dists)
-    return(zip(beer_dists[orders[:topN]], [idx2place[i] for i in orders[:topN]]))
-
-rec = get_place_recomm(1,place_dist, idx2place, topN=10)
-tuple(rec)

aab568771aad4c62acdd5c6da2ee476fb85d2722
leo <lbh6314@gmail.com>
2021-05-13 11:44:44 +0900

    for merge with another laptop
    this is updated on desktop//
    add modify few things

--- travel_recommend/recommend/cal_knn.py
+++ travel_recommend/recommend/cal_knn.py
@@ -6,12 +6,17 @@ from surprise import SVD, accuracy
 from surprise import Reader, Dataset 
 import surprise
 import os
-from .models import Treview
+from .models import Travel, Treview, Usert
 
 
 def Cal_Knn(user_id):
-    results =[]
+    #results =[]
     
+    user = Usert.objects.all()
+    travel = Travel.objects.all()
+    
+    travels = travel.values('tourid', 'city', 'town', 'site', 'genre1', 'genre2','genre3')
+    #print(travels)
     '''
     qs = SomeModel.objects.select_related().filter(date__year=2012)
     q = qs.values('date', 'OtherField')
@@ -20,37 +25,45 @@ def Cal_Knn(user_id):
     
     # 1. raw dataset
     rate  = Treview.objects.all()
-    rates = rate.values('treview_no', 'treview_id', 'tourid', 'rating')
+    rates = rate.values('review_no', 'user_no', 'placeid', 'rating')
     
-    #rating = pd.DataFrame(data = rate, columns=['treview_no', 'treview_id', 'tourid', 'rating'])
+    #rating = pd.DataFrame(data = rate, columns=['treview_no', 'user_no', 'placeid', 'rating'])
     rating = pd.DataFrame.from_records(rates) 
-    rating.head()   #   critic(user)   title(item)   rating
+    #print(rating.head())   #   critic(user)   title(item)   rating
     
     print(user_id)
-    rating['treview_id'].value_counts()
-    rating['tourid'].value_counts()
+    #print('리뷰 id 갯수',rating['user_no'].value_counts())
+    #print('placeid 갯수', rating['placeid'].value_counts())
     
     # 관광 vs 미관광
-    tab = pd.crosstab(rating['treview_id'], rating['tourid'])
+    tab = pd.crosstab(rating['user_no'], rating['placeid'])
+    #print('1')
     #print(tab)
     
     # rating
     # 두 개의 집단변수를 가지고 나머지 rating을 그룹화
-    rating_g = rating.groupby(['treview_id', 'tourid'])
-    rating_g.sum()
+    rating_g = rating.groupby(['user_no', 'placeid'])
+    #rating_g.sum()
     tab = rating_g.sum().unstack() # 행렬구조로 변환
+    print(tab)
     #사용자 2이 가지 않은 곳, 1,15, 39....
+    #print('요기는 일다 ㄴ찍힘')
+
     
+
+    #####요기가 문제#####
     # 2. rating 데이터셋 생성
+    print('실행')
     reader = Reader(rating_scale= (1, 5)) # 평점 범위
     data = Dataset.load_from_df(df=rating, reader=reader)
     # rating이라는 데이터프레임은 reader(1~5)의 평점 범위를 가진다.
-    #print(data)
+    print('2', data)
     
     # 3. train/test set
     train = data.build_full_trainset() # 훈련셋
     test = train.build_testset() # 검정셋
-    
+    print('3', train.head())
+
     # 4. model 생성
     option = {'name': 'pearson'}
     model = surprise.KNNBaseline(sim_options=option)
@@ -58,7 +71,7 @@ def Cal_Knn(user_id):
     
     # 5. user_id 입력
     #user_id = 1 # 추천대상자
-    item_ids = range(0, 2106) # tourid 범위
+    item_ids = range(0, 2106) # placeid 범위
     actual_rating = 0 # 평점
     
     predict_result = []
@@ -70,12 +83,12 @@ def Cal_Knn(user_id):
             predict_result.append(a)
     
     ddff = pd.DataFrame(predict_result)
-    #print(ddff)
+    print(ddff)
     
     # 유저 1 추천 여행지 상위 5개
     result = ddff.sort_values(by='est', ascending=False)[:5]
     
-    result.to_csv()
-    print(result)
-    results.append(result)
+    #result.to_csv()
+    print(result, type(result))
+    #results.append(result)
     return result
--- travel_recommend/recommend/models.py
+++ travel_recommend/recommend/models.py
@@ -14,37 +14,57 @@ travel
 tourid, city, town, name, type1, type2, type3, genre
 
 '''
+#모델을 다시 바꿔야함...
+# 어떻게??
+
+
 
 class Travel(models.Model):
-    tourid = models.IntegerField(db_column='tourId', primary_key=True)  # Field name made lowercase.
-    city = models.CharField(max_length=10, blank=True, null=True)
+    tourid = models.AutoField(primary_key=True)
+    city = models.CharField(max_length=20, blank=True, null=True)
     town = models.CharField(max_length=20, blank=True, null=True)
     site = models.CharField(max_length=50, blank=True, null=True)
-    genre1 = models.CharField(max_length=20,blank=True, null=True)
-    genre2 = models.CharField(max_length=20,blank=True, null=True)
-    genre3 = models.CharField(max_length=20,blank=True, null=True)
-    
+    genre1 = models.CharField(max_length=20, blank=True, null=True)
+    genre2 = models.CharField(max_length=20, blank=True, null=True)
+    genre3 = models.CharField(max_length=20, blank=True, null=True)
+
     class Meta:
         managed = False
         db_table = 'travel'
 
 
 class Treview(models.Model):
-    treview_no = models.AutoField(primary_key=True)
-    treview_id = models.CharField(max_length=20, blank=True, null=True)
-    tourid = models.IntegerField(db_column='tourid', blank=True, null=True)  # Field name made lowercase.
-    rating = models.IntegerField(blank=True, null=True)
-    
+    user_no = models.ForeignKey('Tuser', models.DO_NOTHING, db_column='user_no', blank=True, null=True)
+    placeid = models.ForeignKey('Travel', models.DO_NOTHING, db_column='placeid', blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
+    review_no = models.AutoField(primary_key=True)
+
 
     class Meta:
         managed = False
         db_table = 'treview'
 
-# class Tuser(models.Model):
-#     user_id = models.CharField(primary_key=True, max_length=20)
-#     user_name = models.CharField(max_length=10, blank=True, null=True)
-#     user_pwd = models.CharField(max_length=10, blank=True, null=True)
 
-#     class Meta:
-#         managed = False
-#         db_table = 'tuser'
+class Tuser(models.Model):
+    user_no = models.AutoField(primary_key=True)
+    genre = models.IntegerField(blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    occ = models.IntegerField(blank=True, null=True)
+    gen = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tuser'
+
+
+
+class Usert(models.Model):
+    user_no = models.ForeignKey(Tuser, models.DO_NOTHING, db_column='user_no', blank=True, null=True)
+    userid = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=20)
+    password2 = models.CharField(max_length=20)
+    etc = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'usert'
